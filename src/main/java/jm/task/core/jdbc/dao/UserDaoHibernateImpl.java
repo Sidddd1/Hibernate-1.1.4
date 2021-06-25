83fe93017e1746327a64ef438fefbb7a04a3ca81
Sidddd1 <81307487+Sidddd1@users.noreply.github.com>
2021-06-25 15:43:06 +0300

    2

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -13,7 +13,7 @@ public class UserDaoHibernateImpl  implements UserDao {
 
     @Override
     public void createUsersTable() {
-        System.out.println();
+        System.out.println("Second commit");
     }
 
     @Override
